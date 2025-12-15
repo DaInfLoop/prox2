1c559c93553039ab8466365b281eb3afcc5fdff7
DaInfLoop <github@dainfloop.is-a.dev>
2025-12-15 10:16:49 +0000

    Add an ActionConfirm to the "approve for meta" button

--- lib/main.ts
+++ lib/main.ts
@@ -44,6 +44,7 @@ import {
   MarkdownText,
   PlainText,
   TextSection,
+	ActionConfirm,
 } from "./block_builder";
 
 export const api_config = {
@@ -309,7 +310,13 @@ const getStagingMessageBlocks = (id: number, text: string) => new Blocks([
     new ButtonAction(
         new PlainText(":office: Approve for meta"),
         "approve:meta",
-        "approve:meta"
+        "approve:meta",
+				new ActionConfirm(
+					new PlainText("Approve for meta"),
+					new PlainText(`Are you sure you want to approve this confession for meta?`),
+					new PlainText("Approve"),
+					new PlainText("Deny")
+				)
     ),
   ]),
 ]).render();
